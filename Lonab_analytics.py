56d3db1cb72825dadaa975ca0486c2b5308f9f95
bissagouem-eng <bissagouem@gmail.com>
2025-11-19 08:56:54 +0000

    Create Lonab_analytics.py

---
+++ Lonab_analytics.py
@@ -0,0 +1,8 @@
+
+# Analytics module generated from Colab
+import polars as pl
+
+def get_analytics_data():
+    """Return pre-computed analytics"""
+    # This would be populated with your actual data
+    return {}
